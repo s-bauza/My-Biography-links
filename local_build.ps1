59d78429822a69e10d36eed4a0e2d4443907a339
s-bauza <s.bauza@alumnos.upm.es>
2025-02-17 11:36:36 +0100

    Add local and remote build scripts; remove obsolete build.ps1

--- local_build.ps1
+++ local_build.ps1
@@ -1,10 +1,9 @@
 .\.venv\Scripts\activate
 pip install --upgrade pip
 pip install -r requirements.txt
-reflex init
-$env:API_URL = "https://my-biography-links.up.railway.app" 
-reflex export --frontend-only
 Remove-Item -Recurse -Force public
+reflex init
+reflex export --frontend-only
 Expand-Archive -Path frontend.zip -DestinationPath .\public
 Remove-Item -Force frontend.zip
 deactivate
---
+++ remote_build.ps1
@@ -0,0 +1,10 @@
+.\.venv\Scripts\activate
+pip install --upgrade pip
+pip install -r requirements.txt
+Remove-Item -Recurse -Force public
+reflex init
+$env:API_URL = "https://my-biography-links.up.railway.app" 
+reflex export --frontend-only
+Expand-Archive -Path frontend.zip -DestinationPath .\public
+Remove-Item -Force frontend.zip
+deactivate
