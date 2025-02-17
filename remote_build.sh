263dd7c447d7df396d9d309cb6c0636b7df406d1
s-bauza <s.bauza@alumnos.upm.es>
2025-02-17 12:51:31 +0100

    Replace remote_build.ps1 with remote_build.sh for improved build process

---
+++ remote_build.sh
@@ -0,0 +1,10 @@
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+rm -rf public
+reflex init
+API_URL=https://my-biography-links.up.railway.app reflex export --frontend-only
+unzip frontend.zip -d public
+rm -f frontend.zip
+deactivate
