d4390c172bb31888fff45ffc4e15901cc0f7985b
s-bauza <s.bauza@alumnos.upm.es>
2025-02-17 10:56:12 +0100

    Api url add

--- build.ps1
+++ build.ps1
@@ -2,6 +2,7 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 reflex init
+$env:API_URL = "https://my-biography-links.up.railway.app" 
 reflex export --frontend-only
 Remove-Item -Recurse -Force public
 Expand-Archive -Path frontend.zip -DestinationPath .\public
