ba25592f7efdd73fffd9e184aacc082be1f770a1
s-bauza <s.bauza@alumnos.upm.es>
2025-02-17 11:07:23 +0100

    Add CORS support for local and production environments

--- rxconfig.py
+++ rxconfig.py
@@ -2,4 +2,8 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
+    cors_allow_origins=[
+        "http://localhost:3000/",
+        "https://sbh-links-web.vercel.app/"
+        ],
 )
