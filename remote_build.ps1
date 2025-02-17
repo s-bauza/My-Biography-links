9baf1abf86bd2487d49c047b2f51c07f0619613b
s-bauza <s.bauza@alumnos.upm.es>
2025-02-17 12:40:06 +0100

    Fix frontend.zip extraction path in remote_build.ps1

--- remote_build.ps1
+++ remote_build.ps1
@@ -5,7 +5,8 @@ Remove-Item -Recurse -Force public
 reflex init
 $env:API_URL = "https://my-biography-links.up.railway.app" 
 reflex export --frontend-only
-Write-Host "Directorio actual: $(Get-Location)"
-Expand-Archive -Path frontend.zip -DestinationPath .\public
-Remove-Item -Force frontend.zip
+
+# Usar la ruta correcta para frontend.zip en la raíz del repositorio
+Expand-Archive -Path .\frontend.zip -DestinationPath .\public
+Remove-Item -Force .\frontend.zip
 deactivate
