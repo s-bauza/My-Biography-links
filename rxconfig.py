1c5c9a3ffa40d4a34bd55be4b709fbdf57c3bbeb
s-bauza <s.bauza@alumnos.upm.es>
2025-03-12 00:25:02 +0100

    Add favicon support and remove old favicon file

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
+    favicon="/assets/favicon.ico",
     cors_allow_origins=[
         "http://localhost:3000/",
         "https://sbh-links-web.vercel.app/"
