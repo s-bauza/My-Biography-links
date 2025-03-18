9eef050afe1b676f00b31c7ed6a2e787c6a61594
s-bauza <s.bauza@alumnos.upm.es>
2025-03-18 12:26:22 +0100

    Add backend URL configuration to rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -3,6 +3,7 @@ import reflex as rx
 config = rx.Config(
     app_name="link_bio",
     favicon="/assets/favicon.ico",
+    backend_url="https://my-biography-links.up.railway.app/",
     cors_allow_origins=[
         "http://localhost:3000/",
         "https://sbh-links-web.vercel.app/"
